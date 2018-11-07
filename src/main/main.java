8496822b670b76c25feb1f6bf01639829a118950
chulhun hwang <qewqsa@naver.com>
2018-11-07 12:45:38 +0900

    modify resuert util_1

--- src/main/main.java
+++ src/main/main.java
@@ -14,25 +14,26 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-//		double[][] positions = new double[][] { { 5.0, -6.0 }, { 13.0, -15.0 }, { 21.0, -3.0 }, { 12.4, -21.2 } };
-//		double[] distances = new double[] { 8.06, 13.97, 23.32, 15.31 };
-//
-//		NonLinearLeastSolver solver = new NonLinearLeastSolver(new TrilaterationFunction(positions, distances), new LevenbergMarquardtOptimizer());
-//		Optimum optimum = solver.solve();
-//
-//		// the answer
-//		double[] centroid = optimum.getPoint().toArray();
-//		
-//		System.out.println("Ceter : " + centroid[0]);
-//		System.out.println("Ceter : " + centroid[1]);
-//
-//		// error and geometry information; may throw SingularMatrixException depending the threshold argument provided
-//		RealVector standardDeviation = optimum.getSigma(0);
-//		RealMatrix covarianceMatrix = optimum.getCovariances(0);
+		double[][] positions = new double[][] { { 5.0, -6.0 }, { 13.0, -15.0 }, { 21.0, -3.0 }, { 12.4, -21.2 }, { 12.4, -1.2 } };
+		double[] distances = new double[] { 8.06, 13.97, 23.32, 15.31, 24.26 };
+
+		NonLinearLeastSolver solver = new NonLinearLeastSolver(new TrilaterationFunction(positions, distances), new LevenbergMarquardtOptimizer());
+		Optimum optimum = solver.solve();
+
+		// the answer
+		double[] centroid = optimum.getPoint().toArray();
 		
-		ThreeDistanceToCenterLocation.calc(37.45096, 127.12709, 37.450962, 127.127106, 37.450996, 127.12711, 3.06, 4.69, 3.52);
-		System.out.println("Location : x = " + ThreeDistanceToCenterLocation.lat + " / y = " + ThreeDistanceToCenterLocation.lon);
-		ThreeDistanceToCenterLocation.callTestMethod(37.45097, 127.12709);
+		System.out.println("centroid : { " + centroid[0] + ", " + centroid[1] + " } " );
+
+		// error and geometry information; may throw SingularMatrixException depending the threshold argument provided
+		RealVector standardDeviation = optimum.getSigma(0);
+		System.out.println("Standard : " + standardDeviation.toString());
+		RealMatrix covarianceMatrix = optimum.getCovariances(0);
+		System.out.println("ConvarianceMatrix : " + covarianceMatrix.toString());
+		
+//		ThreeDistanceToCenterLocation.calc(37.45096, 127.12709, 37.450962, 127.127106, 37.450996, 127.12711, 3.06, 4.69, 3.52);
+//		System.out.println("Location : x = " + ThreeDistanceToCenterLocation.lat + " / y = " + ThreeDistanceToCenterLocation.lon);
+//		ThreeDistanceToCenterLocation.callTestMethod(37.45097, 127.12709);
 
 	}
 
